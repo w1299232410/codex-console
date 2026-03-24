4128c527fa65342087cd5e6b1d09f776adab861b
w1299232410 <w1299232410@sina.com>
2026-03-24 15:54:19 +0800

    Replace mail iteration with sorted emails
    Commented out the original loop and replaced it with a sorted version of emails.

--- src/services/freemail.py
+++ src/services/freemail.py
@@ -210,7 +210,8 @@ class FreemailService(BaseEmailService):
                     time.sleep(3)
                     continue
 
-                for mail in mails:
+                #for mail in mails:
+                 for mail in sorted(emails, key=lambda x: x["id"], reverse=True):
                     mail_id = mail.get("id")
                     if not mail_id or mail_id in seen_mail_ids:
                         continue
